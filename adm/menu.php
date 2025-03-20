a2a4f8b484aae81c2531304ab3567d4af01a5793
Joao0liver <joaoolv.profissional@gmail.com>
2025-03-20 20:19:15 -0300

    - Atualização da Sidebar conforme os diferentes níveis de acesso; - Adição dos Módulos de Cliente, Administrador e Motoboy; - Adição dos Menus conforme os módulos; - Comentário do CSS na parte de cores;

---
+++ adm/menu.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once("../layout/header_adm.php");
+
+?>
+
+<!-- Blank Start -->
+            <div class="container-fluid pt-4 px-4">
+                <div class="row vh-100 bg-light rounded align-items-center justify-content-center mx-0">
+                    <div class="col-md-6 text-center">
+                        <h3>Olá, XXXXX...</h3>
+                        <h5>Administrador</h5>
+                    </div>
+                </div>
+            </div>
+<!-- Blank End -->
+
+<?php
+
+include_once("../layout/footer.php");
+
+?>
---
+++ cliente/menu.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once("../layout/header_cliente.php");
+
+?>
+
+<!-- Blank Start -->
+            <div class="container-fluid pt-4 px-4">
+                <div class="row vh-100 bg-light rounded align-items-center justify-content-center mx-0">
+                    <div class="col-md-6 text-center">
+                        <h3>Olá, xxxxxx...</h3>
+                        <h5>Seja bem-vindo ao Rapidinho Frete!</h5>
+                    </div>
+                </div>
+            </div>
+<!-- Blank End -->
+
+<?php
+
+include_once("../layout/footer.php");
+
+?>
